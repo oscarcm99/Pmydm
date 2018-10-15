c0ff31de00ea5b555cef460fcbb1dc64be3609f9
oscarcm99 <gorri0302@gmail.com>
2018-10-15 11:37:50 +0200

    Clase

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 23;
+            Type tipo;
+            if (128 is System.Int32)
+                Console.WriteLine("Si es un numero");
+            else
+                Console.WriteLine("No es un numero");
+            tipo = numero.GetType();
+            Console.WriteLine("El tipo de datos es: "+tipo.ToString());
+            Console.ReadKey();
+        }
+    }
+}
