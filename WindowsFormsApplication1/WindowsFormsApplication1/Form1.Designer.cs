a29774aeadf3aa9d376a88abdc481726a1de0317
oscarcm99 <gorri0302@gmail.com>
2018-10-16 11:48:50 +0200

    CLASE

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class FormPrimera
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calcular = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(75, 59);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(151, 68);
+            this.calcular.TabIndex = 0;
+            this.calcular.Text = "HOLA";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(67, 200);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(231, 47);
+            this.resultado.TabIndex = 1;
+            this.resultado.Text = "Resultado: ";
+            this.resultado.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FormPrimera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(25F, 47F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 488);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.calcular);
+            this.Font = new System.Drawing.Font("Arial", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(9);
+            this.Name = "FormPrimera";
+            this.Text = "01Primera";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label resultado;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormPrimera : Form
+    {
+        public FormPrimera()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] mitabla = { 11, 22, 33, 44, 55, 66 };
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    resultado.Text += " " + mitabla[i];
+            //}
+            foreach (int numero in mitabla ){
+                resultado.Text += " "+numero;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
